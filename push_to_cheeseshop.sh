5eeba2103d738dd3b5c06ebc0eff97c2a40b401f
Ketouem <cyril.thomas@surycat.com>
2017-11-15 10:21:11 +0100

    Updating obsolete package distribution

--- push_to_cheeseshop.sh
+++ push_to_cheeseshop.sh
@@ -1,9 +1,9 @@
 echo -e "[distutils]
 index-servers=pypi
 [pypi]
-repository = https://pypi.python.org/pypi
 username = $PYPI_USER
 password = $PYPI_PASSWORD
 " > ~/.pypirc
 
-python setup.py sdist upload
+python setup.py sdist
+twine upload dist/Flask-Boto3-$CIRCLE_TAG.tar.gz
